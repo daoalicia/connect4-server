6905b48fdce0bfa40e062ecd4d4aabcb66d60009
Alicia Dao <daoalicia@gmail.com>
2020-12-04 20:06:26 -0500

    changed port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // let connection = mysql.createConnection(credentials);
 // connection.connect();
 
-const port = 443;
+const port = 3443;
 app.listen(port, () => {
     console.log(`We're live on port ${port}!`);
 });
