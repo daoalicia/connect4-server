0890110ce45d4bf7b2c0eb87dab3055454fb63f8
nguyenxx <nguyenxx@github.com>
2020-12-07 03:26:55 +0000

    Added schema.sql and make.sql

---
+++ make.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE connect4 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER connect4_user@localhost IDENTIFIED BY 'connect4@ASProject2'
+GRANT ALL PRIVILEGES ON today.* TO today_user@locahost;
---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS board;
+
+CREATE TABLE board (
+  cell_id SERIAL PRIMARY KEY,
+  colNum INT,
+  rowNum INT,
+  isPlayer1 BOOL,
+  isPlayer2 BOOL,
+);
