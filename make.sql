a8d17848e149a563cef167ed4bf8d5c796c46724
nguyenxx <nguyenxx@users.noreply.github.com>
2020-12-06 22:48:45 -0500

    updATE

--- make.sql
+++ make.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS connect4;
-DROP USER IF EXISTS connect4_user@locahost;
+DROP USER IF EXISTS connect4_user@localhost;
 
 CREATE DATABASE connect4 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER connect4_user@localhost IDENTIFIED BY 'connect4@ASProject2';
-GRANT ALL PRIVILEGES ON connect4.* TO connect4_user@locahost;
+GRANT ALL PRIVILEGES ON connect4.* TO connect4_user@localhost;
